f322d1331f4c32cf6c352ff6b098871b37079f18
Cédric HT <altaris@users.noreply.github.com>
2021-10-18 18:14:44 +0900

    CLI

--- src/benchmark.py
+++ src/benchmark.py
@@ -4,8 +4,9 @@
 # To run on a big boy
 
 from pathlib import Path
-import os
 
+import click
+import numpy as np
 from nmoo.benchmark import Benchmark
 from nmoo.denoisers import KNNAvg, ResampleAverage
 from nmoo.evaluators import EvaluationPenaltyEvaluator
@@ -14,7 +15,6 @@ from nmoo.wrapped_problem import WrappedProblem
 from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.problems.multi.zdt import ZDT1
 from pymoo.util.termination.default import MultiObjectiveDefaultTermination
-import numpy as np
 
 from c_region_simulator_problem import CRegionSimulatorProblem
 from pendulum_cart_problem import PendulumCartProblem
@@ -27,6 +27,91 @@ C_REGION_SIMULATOR_PATH = (
 OUTPUT_DIR_PATH = (
     Path(__file__).absolute().parent / ".." / "results" / "benchmark2"
 )
+
+
+@click.group()
+def main() -> None:
+    pass
+
+
+@main.command()
+def consolidate() -> None:
+    benchmark = make_benchmark()
+    benchmark._consolidate_pair_results()
+    benchmark._compute_performance_indicators()
+    benchmark.dump_results(
+        benchmark._output_dir_path / "benchmark.csv", index=False
+    )
+
+
+def make_benchmark() -> Benchmark:
+    """Defines the benchmark"""
+    # The noisy problems and their names
+    noisy_problems = {
+        "crs_8": make_c_region_simulator(8),
+        "pd_5": make_pendulum_cart(5),
+        "zdt1_30": make_zdt1(30, 0.2),
+    }
+    # The knn-wrapped problems descriptions
+    knn_problems = {
+        f"{name}_k{k}": {
+            "problem": KNNAvg(
+                distance_weight_type="squared",
+                max_distance=0.5,
+                n_neighbors=k,
+                problem=problem,
+            )
+        }
+        for name, problem in noisy_problems.items()
+        for k in [10, 25, 50, 100, 1000]
+    }
+    # The resample-wrapped problems descriptions
+    avg_problems = {
+        f"{name}_a{n}": {
+            "evaluator": EvaluationPenaltyEvaluator("times", n),
+            "problem": ResampleAverage(
+                problem=problem,
+                n_evaluations=n,
+            ),
+        }
+        for name, problem in noisy_problems.items()
+        for n in [10, 100]
+    }
+    # All the problem descriptions
+    problems = {**knn_problems, **avg_problems}
+
+    # Add additional problem data
+    for name, problem in problems.items():
+        problem["df_n_evals"] = 10
+        if name.startswith("crs_8"):
+            problem["hv_ref_point"] = np.array([100.0, 10.0])
+        elif name.startswith("pd_5"):
+            problem["hv_ref_point"] = np.array([10.0, 10.0])
+        elif name.startswith("zdt1_30"):
+            problem["hv_ref_point"] = np.array([10.0, 10.0])
+        pf = problem["problem"].ground_problem().pareto_front()
+        if not (pf is None):
+            problem["pareto_front"] = pf
+
+    # All algorithm descriptions
+    algorithms = {
+        f"nsga2_p{pop_size}": {
+            "algorithm": NSGA2(pop_size=pop_size),
+            "termination": MultiObjectiveDefaultTermination(
+                n_max_evals=100_000,
+            ),
+        }
+        for pop_size in [10, 20, 50]
+    }
+
+    return Benchmark(
+        algorithms=algorithms,
+        max_retry=10,
+        n_runs=30,
+        output_dir_path=OUTPUT_DIR_PATH,
+        performance_indicators=["df", "hv", "igd"],
+        problems=problems,
+    )
 
 
 def make_c_region_simulator(n_dimensions: int) -> CRegionSimulatorProblem:
@@ -99,86 +184,23 @@ def make_zdt1(n_var: int, noise: float) -> ZDT1:
     )
 
 
-def main():
-    # The noisy problems and their names
-    noisy_problems = {
-        "crs_8": make_c_region_simulator(8),
-        "pd_5": make_pendulum_cart(5),
-        "zdt1_30": make_zdt1(30, 0.2),
-    }
-    # The knn-wrapped problems descriptions
-    knn_problems = {
-        f"{name}_k{k}": {
-            "problem": KNNAvg(
-                distance_weight_type="squared",
-                max_distance=0.5,
-                n_neighbors=k,
-                problem=problem,
-            )
-        }
-        for name, problem in noisy_problems.items()
-        for k in [10, 25, 50, 100, 1000]
-    }
-    # The resample-wrapped problems descriptions
-    avg_problems = {
-        f"{name}_a{n}": {
-            "evaluator": EvaluationPenaltyEvaluator("times", n),
-            "problem": ResampleAverage(
-                problem=problem,
-                n_evaluations=n,
-            ),
-        }
-        for name, problem in noisy_problems.items()
-        for n in [10, 100]
-    }
-    # All the problem descriptions
-    problems = {**knn_problems, **avg_problems}
-
-    # Add additional problem data
-    for name, problem in problems.items():
-        problem["df_n_evals"] = 10
-        if name.startswith("crs_8"):
-            problem["hv_ref_point"] = np.array([100.0, 10.0])
-        elif name.startswith("pd_5"):
-            problem["hv_ref_point"] = np.array([10.0, 10.0])
-        elif name.startswith("zdt1_30"):
-            problem["hv_ref_point"] = np.array([10.0, 10.0])
-        pf = problem["problem"].ground_problem().pareto_front()
-        if not (pf is None):
-            problem["pareto_front"] = pf
-
-    # All algorithm descriptions
-    algorithms = {
-        f"nsga2_p{pop_size}": {
-            "algorithm": NSGA2(pop_size=pop_size),
-            "termination": MultiObjectiveDefaultTermination(
-                n_max_evals=100_000,
-            ),
-        }
-        for pop_size in [10, 20, 50]
-    }
-
-    # Benchmark definition and execution
-    if not os.path.isdir(OUTPUT_DIR_PATH):
-        os.mkdir(OUTPUT_DIR_PATH)
+@main.command()
+@click.option("--n-jobs", default=60)
+def run(n_jobs: int) -> None:
     restart = True
     while restart:
         try:
-            benchmark = Benchmark(
-                algorithms=algorithms,
-                max_retry=10,
-                n_runs=30,
-                output_dir_path=OUTPUT_DIR_PATH,
-                performance_indicators=["df", "hv", "igd"],
-                problems=problems,
-            )
-            benchmark.run(n_jobs=60, verbose=50)
+            benchmark = make_benchmark()
+            benchmark.run(n_jobs=n_jobs, verbose=50)
+        except KeyboardInterrupt:
+            restart = False
         except:
             print("=============================")
             print("Benchmark crashed, restarting")
             print("=============================")
         else:
             restart = False
+
 
 if __name__ == "__main__":
     main()
